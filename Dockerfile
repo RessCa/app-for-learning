073d1519c836e1459139da62e82a501b1a08993c
ressca <thinkpad@DESKTOP-7V7DK07.localdomain>
2025-10-04 22:48:39 +0200

    containers fully working

--- Dockerfile
+++ Dockerfile
@@ -3,19 +3,22 @@ WORKDIR /app
 
 RUN apt update && apt install -y maven && rm -rf /var/lib/apt/lists/*
 
-#Downloads and cache dependencies
 COPY pom.xml .
+#Downloads and cache dependencies
 RUN mvn dependency:go-offline -B
 
 #Builds Jar file
 COPY src ./src
 RUN mvn clean package
 
-#Copies bilded Jar file
+#Builds final container
 FROM eclipse-temurin:21-jre
 
 WORKDIR /app
 
+#netcat checks if database is running
+RUN apt update && apt install -y netcat-openbsd && rm -rf /var/lib/apt/lists/*
+
 COPY --from=build /app/target/app-for-learning-1.0-SNAPSHOT.jar app.jar
 
-ENTRYPOINT ["java", "-jar", "app.jar"]
+ENTRYPOINT ["sh", "-c", "until nc -z db 5432; do echo waiting for db; sleep 1; done; java -jar app.jar"]
--- src/main/java/org/resso/Main.java
+++ src/main/java/org/resso/Main.java
@@ -1,6 +1,5 @@
 package org.resso;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -8,49 +7,46 @@ import org.hibernate.cfg.Configuration;
 
 public class Main {
     public static void main(String[] args) {
-        //Loads environment variables
-//        Dotenv dotenv = Dotenv.load();
-//
-//        final String url = String.format(
-//                "jdbc:postgresql://%s:%s/%s",
-//                dotenv.get("DB_HOST"),
-//                dotenv.get("DB_PORT"),
-//                dotenv.get("POSTGRES_DB"));
-//
-//        //JDBC configuration
-//        Configuration cfg = new Configuration().configure();
-//        cfg.setProperty("hibernate.connection.url", url);
-//        cfg.setProperty("hibernate.connection.username", dotenv.get("POSTGRES_USER"));
-//        cfg.setProperty("hibernate.connection.password", dotenv.get("POSTGRES_PASSWORD"));
-//
-//
-//        Developer dev1 = new Developer("Bob", 22, "JS");
-//        Developer dev2 = new Developer("Rob", 32, "Swift");
-//
-//        Computer comp1 = new Computer("PC", "intel i9 11900", "GeForce 1660", 16, 256);
-//        Computer comp2 = new Computer("Laptop", "intel i7 2700", "GeForce 660", 16, 516);
-//
-//        dev1.addComputer(comp1);
-//        dev2.addComputer(comp2);
-//
-//        SessionFactory sessionFactory = cfg.buildSessionFactory();
-//
-//        Session session = sessionFactory.openSession();
-//        session.beginTransaction();
-//
-//        //Developer dev1 = session.find(Developer.class, 1);
-//
-//        session.persist(dev1);
-//        session.persist(dev2);
-//
-//        System.out.println(dev1);
-//        System.out.println(dev2);
-//
-//        session.getTransaction().commit();
-//
-//        session.close();
-//        sessionFactory.close();
 
-        System.out.println("Hello World!");
+        final String url = String.format(
+                "jdbc:postgresql://%s:%s/%s",
+                System.getenv("DB_HOST"),
+                System.getenv("DB_PORT"),
+                System.getenv("POSTGRES_DB"));
+
+        //JDBC configuration
+        Configuration cfg = new Configuration().configure();
+        cfg.setProperty("hibernate.connection.url", url);
+        cfg.setProperty("hibernate.connection.username", System.getenv("POSTGRES_USER"));
+        cfg.setProperty("hibernate.connection.password", System.getenv("POSTGRES_PASSWORD"));
+
+
+        Developer dev1 = new Developer("Bob", 22, "JS");
+        Developer dev2 = new Developer("Rob", 32, "Swift");
+
+        Computer comp1 = new Computer("PC", "intel i9 11900", "GeForce 1660", 16, 256);
+        Computer comp2 = new Computer("Laptop", "intel i7 2700", "GeForce 660", 16, 516);
+
+        dev1.addComputer(comp1);
+        dev2.addComputer(comp2);
+
+        SessionFactory sessionFactory = cfg.buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+//        Developer dev1 = session.find(Developer.class, 1);
+//        Developer dev2 = session.find(Developer.class, 2);
+
+        session.persist(dev1);
+        session.persist(dev2);
+
+        System.out.println(dev1);
+        System.out.println(dev2);
+
+        session.getTransaction().commit();
+
+        session.close();
+        sessionFactory.close();
     }
 }
